222580e91c02bd2c09b9c0a8ce8cc6fbfdbadd08
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-08-16 01:06:26 +0200

    feat(routes): ajouter des routes pour la gestion des villes et des utilisateurs
    Ajout de nouvelles routes dans le fichier api.php pour gérer les opérations CRUD des entités City et User. Ces modifications permettent d'améliorer l'API en offrant des endpoints pour lister, créer, afficher, mettre à jour et supprimer des villes et des utilisateurs, facilitant ainsi la gestion des données associées.

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,29 @@
 <?php
 
-use App\Http\Controllers\Api\V1\welcomeController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\CityController;
+use App\Http\Controllers\API\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Prefix routes 
+// Prefix routes
 Route::prefix('v1')->group(function () {
-    
-    //welcome
-    Route::controller(welcomeController::class)->group(function () {
+
+
+    // City
+    Route::controller(CityController::class)->group(function () {
+        Route::get('/cities', 'index');
+        Route::get('/cities/{id}', 'show');
+        Route::post('/cities', 'store');
+        Route::put('/cities/{id}', 'update');
+        Route::delete('/cities/{id}', 'destroy');
+    });
+
+    // User
+    Route::controller(UserController::class)->group(function () {
         Route::get('/users', 'index');
         Route::get('/users/{id}', 'show');
+        Route::post('/users', 'store');
+        Route::put('/users/{id}', 'update');
+        Route::delete('/users/{id}', 'destroy');
     });
 
 
