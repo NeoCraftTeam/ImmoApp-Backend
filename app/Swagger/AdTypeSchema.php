b2ef527dd2310cc11cbc9eab84fc932ac18e68dc
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-08-19 00:12:35 +0200

    feat(api): ajouter le schéma AdType et les routes associées
    Création du schéma AdType pour la documentation OpenAPI, incluant les propriétés id, name et desc. Ajout des routes pour gérer les types d'annonces, permettant de récupérer, créer, afficher, mettre à jour et supprimer des types d'annonces. Ces modifications améliorent la clarté et l'accessibilité de l'API pour la gestion des types d'annonces.

---
+++ app/Swagger/AdTypeSchema.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\Schema(
+ *     schema="AdType",
+ *     type="object",
+ *     title="AdType",
+ *     description="Modèle pour type d'annonce",
+ *     @OA\Property(property="id", type="integer"),
+ *      @OA\Property(property="name", type="string"),
+ *      @OA\Property(property="desc", type="string"),
+ * )
+ */
+class AdTypeSchema
+{
+}
