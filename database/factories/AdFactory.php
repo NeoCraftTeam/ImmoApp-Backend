a2b3db9c5a797ecdfec6b11bb75a2d4b345a099a
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-08-19 00:07:43 +0200

    refactor(factories): améliorer la structure des usines d'annonces et de paiements
    Modification des usines AdFactory et PaymentFactory pour améliorer la lisibilité et la cohérence du code. Changement de la gestion des coordonnées géographiques dans AdFactory pour utiliser un format de point, et ajustement de l'indentation dans PaymentFactory. Ces améliorations renforcent la clarté et la maintenabilité des usines de données.

--- database/factories/AdFactory.php
+++ database/factories/AdFactory.php
@@ -3,9 +3,9 @@
 namespace Database\Factories;
 
 use App\Models\Ad;
+use App\Models\AdType;
 use App\Models\Quarter;
 use App\Models\User;
-use App\Models\AdType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
@@ -18,18 +18,19 @@ class AdFactory extends Factory
     public function definition(): array
     {
         $title = $this->faker->sentence();
+        $latitude = $this->faker->latitude();      // entre -90 et 90
+        $longitude = $this->faker->longitude();    // entre -180 et 180
         return [
             'title' => $title,
             'slug' => Str::slug($title),
             'description' => $this->faker->paragraph(),
             'adresse' => $this->faker->address(),
             'price' => $this->faker->numberBetween(25000, 150000), // Random number with 5 digits
-            'surface_area' => $this->faker->randomNumber(5, false ),
+            'surface_area' => $this->faker->randomNumber(5, false),
             'bedrooms' => $this->faker->randomDigitNotNull(),
             'bathrooms' => $this->faker->randomDigitNotNull(),
             'has_parking' => $this->faker->boolean(),
-            'latitude' => $this->faker->latitude(),
-            'longitude' => $this->faker->longitude(),
+            'location' => "POINT($longitude $latitude)",
             'status' => $this->faker->randomElement(['available', 'reserved', 'rent']),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
@@ -37,7 +38,7 @@ class AdFactory extends Factory
             'user_id' => User::factory(),
             'quarter_id' => Quarter::factory(),
             // Each ad is limked to a type
-            'type_id'      => AdType::inRandomOrder()->first()->id ?? AdType::factory(),
+            'type_id' => AdType::inRandomOrder()->first()->id ?? AdType::factory(),
         ];
     }
 }
--- database/factories/CityFactory.php
+++ database/factories/CityFactory.php
@@ -2,9 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\Ad;
 use App\Models\City;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 
