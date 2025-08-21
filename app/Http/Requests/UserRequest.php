330dc601a8aa2c7d2115c396b35e989f97f53f4c
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-08-21 22:39:48 +0200

    feat(request): améliorer les règles de validation des utilisateurs
    Modification des règles de validation dans UserRequest pour gérer les méthodes POST, PUT et PATCH. Les règles incluent désormais la confirmation du mot de passe lors de la création d'un utilisateur et des validations conditionnelles pour les mises à jour. Ces changements améliorent la flexibilité et la sécurité des données utilisateur lors des opérations de création et de mise à jour.

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -18,21 +19,35 @@ class UserRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
-        return [
-            'firstname' => ['required', 'string', 'max:255'],
-            'lastname' => ['required', 'string', 'max:255'],
-            'phone_number' => ['required', 'string', 'regex:/^\+?[0-9]{7,20}$/'],
-            'email' => ['required', 'email', 'max:255', Rule::unique('user', 'email')->ignore($this->user()?->id)], // if the user is connected, ignore their own email
-            'password' => ['required', 'string', 'min:8'],
-            'avatar' => ['required', 'image', 'max:2048'],
-            'role' => ['required', 'string'],
-            'type' => ['nullable', 'string'],
-            'city_id' => ['required', 'integer', 'exists:city,id'],
-        ];
+        if ($this->isMethod('post')) {
+            return [
+                'firstname' => ['required', 'string', 'max:255'],
+                'lastname' => ['required', 'string', 'max:255'],
+                'phone_number' => ['required', 'string', 'regex:/^\+?[0-9]{7,20}$/'],
+                'email' => ['required', 'email', 'max:255', Rule::unique('user', 'email')->ignore($this->user()?->id)], // if the user is connected, ignore their own email
+                'password' => ['required', 'string', 'min:8', 'confirmed:confirm_password'],
+                'role' => ['required', 'string'],
+                'type' => ['nullable', 'string'],
+                'city_id' => ['required', 'integer', 'exists:city,id'],
+            ];
+        }
+        if ($this->isMethod('put') || $this->isMethod('patch')) {
+            return [
+                'firstname' => ['sometimes', 'string', 'max:255'],
+                'lastname' => ['sometimes', 'string', 'max:255'],
+                'phone_number' => ['sometimes', 'string', 'regex:/^\+?[0-9]{7,20}$/'],
+                'email' => ['sometimes', 'email', 'max:255', Rule::unique('user', 'email')->ignore($this->user()?->id)], // if the user is connected, ignore their own email
+                'password' => ['sometimes', 'string', 'min:8'],
+                'role' => ['sometimes', 'string'],
+                'type' => ['nullable', 'string'],
+                'city_id' => ['sometimes', 'integer', 'exists:city,id'],
+            ];
+        }
+        return [];
     }
 
     public function messages(): array
@@ -47,8 +62,6 @@ class UserRequest extends FormRequest
             'password.required' => 'Le mot de passe est obligatoire.',
             'password.min' => 'Le mot de passe doit comporter au moins 8 caractères.',
             'password.confirmed' => 'Le mot de passe et sa confirmation ne correspondent pas.',
-            'avatar.required' => "L'avatar est obligatoire.",
-            'role.required' => "Le rôle est obligatoire.",
             'city_id.required' => "La ville est obligatoire.",
         ];
     }
