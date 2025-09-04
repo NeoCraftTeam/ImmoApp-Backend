4fac30e30f7d1b8fdc4ea058b614c9be183ff328
cedrickdev <cedrickfeze24@gmail.com>
2025-09-04 16:07:14 +0000

    feat(cors): update CORS configuration for authentication routes
    - Added new authentication routes to the CORS 'paths' configuration to allow access for login, registration, and user management.
    - Cleaned up allowed origins by removing redundant URL and adjusted max_age for performance optimization.

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api/v1/auth/login', 'api/v1/auth/registerCustomer', 'api/v1/auth/registerAgent', 'api/v1/auth/registerAdmin', 'api/v1/auth/logout', 'api/v1/auth/refresh', 'api/v1/auth/me'],
 
     'allowed_methods' => ['*'],
 
@@ -23,19 +23,17 @@ return [
         'http://localhost:3000', // front
         'http://localhost:8080', // localhost
         'http://localhost:5173', // localhost
-        'https://keyhome.neocraft.dev/https://keyhome.neocraft.dev' // prod
+        'https://keyhome.neocraft.dev', // prod
+        'http://127.0.0.1:3000',
     ],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [
-        'X-Total-Count',
-        'X-Pagination-Count',
-    ],
+    'exposed_headers' => [],
 
-    'max_age' => 86400,  // 24 heures (améliore les performances)
+    'max_age' => 0,  // 24 heures (améliore les performances)
 
     'supports_credentials' => true,
 
