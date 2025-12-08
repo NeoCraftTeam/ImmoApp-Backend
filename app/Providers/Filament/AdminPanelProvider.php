b317c356c37dbaf0033f12c97ef10310c40b97e1
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-12-08 17:33:05 +0100

    feat(filament): disable global search and remove default widgets
    - Disabled the global search functionality for improved customization.
    - Removed `AccountWidget` and `FilamentInfoWidget` from default widgets, marking them as comments for potential future use.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -13,8 +13,6 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -42,6 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ->passwordReset()
             ->emailVerification()
             ->emailChangeVerification()
+            ->globalSearch(false)
             ->profile(EditProfile::class)
             ->sidebarCollapsibleOnDesktop()
             ->font('poppins')
@@ -58,8 +57,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Admin/Widgets'), for: 'App\Filament\Admin\Widgets')
             ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
+                // AccountWidget::class,
+                // FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
