92e52973b7ea3d0b6e95aa3081ff455f63b14705
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-12-13 02:04:19 +0100

    refactor(admin): enhance QuarterResource navigation properties
    - Added `navigationGroup`, `navigationLabel`, and `modelLabel` properties to the QuarterResource class for improved organization and clarity in the admin panel.
    - This change enhances the user interface by providing clearer labels and grouping for quarter-related resources.

--- app/Filament/Admin/Resources/Quarters/QuarterResource.php
+++ app/Filament/Admin/Resources/Quarters/QuarterResource.php
@@ -33,9 +33,15 @@ class QuarterResource extends Resource
 {
     protected static ?string $model = Quarter::class;
 
+    protected static string|null|\UnitEnum $navigationGroup = 'Villes & Quartiers';
+
     protected static string|BackedEnum|null $navigationIcon = Heroicon::BuildingOffice2;
 
     protected static ?string $recordTitleAttribute = 'name';
+
+    protected static ?string $navigationLabel = 'Quartiers';
+
+    protected static ?string $modelLabel = 'Quartier';
 
     public static function form(Schema $schema): Schema
     {
